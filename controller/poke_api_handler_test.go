775103a3cbbb245ed137ac11eb0075409d725b6d
camilaleniss <mlmariacami2@gmail.com>
2022-06-08 01:37:28 -0500

    Add testing for endpoint

--- controller/poke_api_handler_test.go
+++ controller/poke_api_handler_test.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
 )
@@ -97,4 +100,36 @@ func TestGetPokemonFromPokeApiInternalErrorWithMocks(t *testing.T) {
 	_, err = GetPokemonFromPokeApi(id)
 	c.NotNil(err)
 	c.EqualError(ErrPokeApiFailure, err.Error())
+}
+
+func TestGetPokemon(t *testing.T) {
+	c := require.New(t)
+
+	r, _ := http.NewRequest("GET", "/pokemon/{id}", nil)
+	w := httptest.NewRecorder()
+
+	//Hack to try to fake gorilla/mux vars
+	vars := map[string]string{
+		"id": "bulbasaur",
+	}
+
+	r = mux.SetURLVars(r, vars)
+
+	GetPokemon(w, r)
+
+	expectedBodyResponse, err := ioutil.ReadFile("samples/parsed_response.json")
+	c.NoError(err)
+
+	var expectedPokemon models.Pokemon
+
+	err = json.Unmarshal([]byte(expectedBodyResponse), &expectedPokemon)
+	c.NoError(err)
+
+	var actualPokemon models.Pokemon
+
+	err = json.Unmarshal(w.Body.Bytes(), &actualPokemon)
+	c.NoError(err)
+
+	c.Equal(http.StatusOK, w.Code)
+	c.Equal(expectedPokemon, actualPokemon)
 }
